ba9f53fa1f669494d69fd2f4ec4a02754f7b1b75
mark <142792645+Marky00000@users.noreply.github.com>
2026-03-09 11:49:24 +0800

    3/9/2026

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,11 @@
+protected $proxies = '*';
+
+protected $headers =
+    \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+    \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+    \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+    \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO;
+
+protected $proxies = '*';
+
+protected $headers = \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL;
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Prefetch Vite assets
         Vite::prefetch(concurrency: 3);
+        
+        // Force HTTPS in production
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
